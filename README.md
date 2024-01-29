ff00ebc6882f0ac3d02f8f93405d874dbc15d6e6
Shchyrba Olga <olga.binder@gmail.com>
2024-01-29 21:47:57 +0200

    gallery

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+  Unsplash API
+  Access Key:vL87H0P4TQegySgYx-Q6wVj7Vaqa-T0bAh0RU3i5WVg
+  Secret key:y4Ft4_GQiUQAb35PiEGhUEfFNliiNuR62Ew_V4anVyg
+  Application ID: 559596
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fetchPhotosWithTopic } from "./gallery-api";
 
 import "./App.css";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+import { ImageGallery } from "./components/ImageGallery/ImageGallery";
 
 function App() {
-  return <></>;
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      try {
+        setLoading(true);
+        const data = await fetchPhotosWithTopic();
+        setPhotos(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    }
+    fetchPhotos();
+  }, []);
+  return (
+    <>
+      <SearchBar />
+      {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {photos.length > 0 && <ImageGallery photos={photos} />}
+    </>
+  );
 }
 
 export default App;
