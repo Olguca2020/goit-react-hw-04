2553f3630c8f77aa18a83f8f6dc528e5de1b8543
Shchyrba Olga <olga.binder@gmail.com>
2024-01-30 07:10:04 +0200

    style

--- src/gallery-api.js
+++ src/gallery-api.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = "https://api.unsplash.com/";
 const clientId = "vL87H0P4TQegySgYx-Q6wVj7Vaqa-T0bAh0RU3i5WVg";
 export const fetchPhotosWithTopic = async (topic) => {
   const response = await axios.get(
-    `search/collections?query=${topic}&client_id=${clientId}&id=1319040&per_page=12&orientation=portrait`
+    `search/collections?query=${topic}&client_id=${clientId}&orientation=portrait`
   );
   console.log(response.data.results);
   return response.data.results;
