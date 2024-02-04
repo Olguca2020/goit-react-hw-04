f5f0367898efc249b6b7434212c94b60214728eb
Shchyrba Olga <olga.binder@gmail.com>
2024-02-04 15:48:24 +0200

    modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,11 +1,12 @@
 import Modal from "react-modal";
-import css from "./ImageModal.css";
+import css from "./ImageModal.module.css";
 
 const customStyles = {
   content: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center", // Додайте цей стиль
     top: "50%",
     left: "50%",
     right: "auto",
@@ -29,9 +30,9 @@ export const ImageModal = ({ isOpen, onRequestClose, photo }) => {
         src={photo.cover_photo.urls.full}
         alt={photo.cover_photo.alt_description}
       />
-      <p>Author: {photo.user.name.toUpperCase()}</p>
-      <p>Likes: {photo.user.total_likes}</p>
-      <button onClick={onRequestClose}>Close Modal</button>
+      {/* <p>Author: {photo.user.name.toUpperCase()}</p>
+      <p>Likes: {photo.user.total_likes}</p> */}
+      {/* <button onClick={onRequestClose}>Close Modal</button> */}
     </Modal>
   );
 };
